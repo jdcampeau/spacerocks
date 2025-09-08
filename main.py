91feb2c01838849d6581b0521fcb78b7dbc9645b
jdcampeau <jdcgithub@duck.com>
2025-09-08 07:12:09 -0500

    added max fps

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
         pygame.display.flip()
+        newdt = clock.tick(60)
+        dt = newdt / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
