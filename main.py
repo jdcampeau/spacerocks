9274bbd686e17c0e32cffd74246328711947ce47
jdcampeau <jdcgithub@duck.com>
2025-09-09 06:29:04 -0500

    added player rotation and movement

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         newdt = clock.tick(60)
         dt = newdt / 1000
